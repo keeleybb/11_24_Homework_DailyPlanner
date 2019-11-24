a2040b5214320ee4cac146334343cc043e3120ff
kbb <54555434+keeleybb@users.noreply.github.com>
2019-11-23 19:20:06 -0700

    update js and responsive

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,118 +1,85 @@
 var jumboDate = moment().format("dddd, MMMM D YYYY");
-var currentHour = (moment().format("HH"));
+var currentHour = (moment().format("HH")-8);
 $("#currentDay").text(jumboDate);
 console.log(moment().format("MM ddd, YYYY hh:mm:ss a"));
 console.log(moment().format("dddd, MMMM D YYYY"));
 
-
-var hourDisplay = 9;
-
 var newTable = $("<table>");
 newTable.addClass("plannerTB");
+
 // var timeHr = 9:00:00;  //h:mm:ss a
+//Arrays to add data and classes to table
 var scheduleHr = ["9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM"];
 var compareHr = ["09", "10", "11", "12", "13", "14", "15", "16", "17"];
+var localHr = ["0900", "1000", "1100", "1200", "100", "200", "300", "400", "500"];
 
-for (i = 0; i< 9; i++) {
+
+for (i = 0; i< scheduleHr.length; i++) {
     var newRow = $("<tr>");
     newRow.addClass("row");
 
-
     var newHour = $("<td>");
     newHour.addClass("hour");
-    newHour.attr("data-name", i);
     newHour.html(scheduleHr[i]);
     
     var newMeetingTD = $("<td>");
     newMeetingTD.addClass("column");
     newMeetingTD.addClass(compareHr[i]);
     newMeetingTD.attr("data-name", compareHr[i]);
-    newMeetingTD.attr("data-time", i);
     newMeetingTD.html("");
     var newMeetingArea = $("<textarea>");
-    newMeetingArea.addClass("textarea, " + i);
-
+    newMeetingArea.addClass("textarea " + localHr[i]);
     
-    var newBtn = $("<td>");
+    var buttonTd = $("<td>");
+    buttonTd.addClass("buttonBox")
+    var newBtn = $("<button>");
     newBtn.addClass("saveBtn");
-    newBtn.attr("data-name", i);
+    newBtn.attr("data-name", localHr[i]);
     newBtn.addClass("saveBtn");
-
     newBtn.html('<i class="fas fa-lock"></i>');
 
     $(".container").append(newTable);
     newTable.append(newRow);
-    newRow.append(newHour, newMeetingTD, newBtn);
-    newMeetingTD.append(newMeetingArea);
-    hourDisplay++;
-    // makeColor();
-    // timeHr++;
-    // console.log(timeHr);
+    newRow.append(newHour, newMeetingTD, buttonTd); //Add all the tds
+    newMeetingTD.append(newMeetingArea); //add the textarea to td
+    buttonTd.append(newBtn); //add the button to td
 }
 
 $('.saveBtn').on("click", function(){
     event.preventDefault();
     console.log(this);
-    var getId = $(this).data("name"); //Get the data-name to use as the local storage key
+    var getId = $(this).data("name"); //Get the data-name to use as the local storage key ex 0900
     console.log(getId);
     var meeting = $("." + getId).val();
     console.log("meeting", meeting);
     localStorage.setItem(getId, meeting);
     renderMeetings();
-    console.log(currentHour);
 })
 
 function renderMeetings() {
     $("textarea").empty();
-    console.log("Did we Get here?");
-    for (i=0; i<9; i++){
-        var returnMeeting = localStorage.getItem(i);
+    console.log("Render Meetings");
+    for (i=0; i < scheduleHr.length; i++){
+        var returnMeeting = localStorage.getItem(localHr[i]);
         console.log(returnMeeting);
-        $("." + i + "").html(returnMeeting);
+        $("." + localHr[i] + "").html(returnMeeting);
     }
 
 }
 renderMeetings();
 
-// $(document).on("click", ".saveBtn", sendMeeting);
-// var colorCurrent = $(".column").attr("data-name");
-// var colorCurrentParse = parseInt(colorCurrent);
+$(".column").each(function(){
+    var dataName = $(this).attr("data-name");
+    var currentTD = "." + dataName;
+    if (dataName == currentHour){
+        console.log(dataName, currentHour, "red", "future");
+        $(currentTD).addClass("present");
+    } else if(dataName < currentHour){
+        console.log(dataName, currentHour, "grey", "past");
+        $(currentTD).addClass("past");
+    } else if (dataName > currentHour){
+        console.log(dataName, currentHour, "green", "future");
+        $(currentTD).addClass("future");
+    }
+});
 
-// for (i=0; i<9; i++){
-//     var dataName = i;
-//         console.log(dataName);
-//         if (i === currentHour){
-//                     // newMeetingTD.addClass("present");
-
-//                     newMeetingTD.addClass("present");
-//         }
-
-
-//     };
-
-    $(".column").each(function(){
-            var dataName = $(this).attr("data-name");
-            var currentTD = "." + dataName;
-            if (dataName == currentHour){
-                console.log(dataName, currentHour, "red", "future");
-                $(currentTD).addClass("present");
-            } else if(dataName < currentHour){
-                console.log(dataName, currentHour, "grey", "past");
-                $(currentTD).addClass("past");
-            } else if (dataName > currentHour){
-                console.log(dataName, currentHour, "green", "future");
-                    $(currentTD).addClass("future");
-
-                }
-        
-        
-        
-
-    })
-
-//     console.log(parseInt($(".column").attr("data-name")));
-//     if (parseInt($(".column").attr("data-name")) === currentHour) {
-//         newMeetingTD.addClass("present");
-//     }
-
-// }
